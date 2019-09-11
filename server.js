2c813940e5aadb578c570727222762917561dc43
John Faber Florez Vasco <johnflorez@MacBook-Air-de-Elizabeth.local>
2019-09-10 22:32:06 -0500

    Manipulando query y body

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-//Usamos la librería express para crear nuestro servidor
+//Observando como funciona el body y query
 const express = require('express');
-//Gestionar las diferentes peticiones, router
-//Permitir separar cabeceras y métodos, peticiones al máxmio
+//Body.parser nos permite trabajar con el body de la petición de forma sencilla
+//npm i body-parser
+const bodyParser = require('body-parser');
 const router = express.Router()
-//Inicializando nuestro servidor
-const app = express();
 
-//Añadir router a la aplicacion de express
+const app = express();
+//tipos de body que quiero enviar
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
-//Peticiones Get
+
+
 router.get('/message', (req, res) =>{
     res.send('Lista de mensajes')
 });
-//Peticiones Post
-router.post('/message', (req, res) =>{
-    res.send('Mensaje añadido')
+//El body siempre lo encontramos en el request
+router.delete('/message', (req, res) =>{
+    //trabajanco con el cuerpo
+    console.log(req.body)
+    //acceder a los parametros por query
+    //http://localhost:3001/message?orderBy=id&age=26
+    console.log(req.query)
+    res.send('Mensaje '+ req.body.name + ' añadido correctamente');
 });
 
 
