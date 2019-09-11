e2759e2452cfb00b9feab36617c421f9e7047df1
John Faber Florez Vasco <johnflorez@MacBook-Air-de-Elizabeth.local>
2019-09-10 22:08:56 -0500

    Probando rutas

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 //Usamos la librería express para crear nuestro servidor
 const express = require('express');
+//Gestionar las diferentes peticiones, router
+//Permitir separar cabeceras y métodos, peticiones al máxmio
+const router = express.Router()
 //Inicializando nuestro servidor
 const app = express();
-//para devolver algo....
-app.use('/', (req, res)=>{
-    res.send("Hola");
+
+//Añadir router a la aplicacion de express
+app.use(router);
+//Peticiones Get
+router.get('/message', (req, res) =>{
+    res.send('Lista de mensajes')
 });
+//Peticiones Post
+router.post('/message', (req, res) =>{
+    res.send('Mensaje añadido')
+});
+
+
 //Ejecutar nuestra aplicación de express (Utilizar un puerto) -> Por donde va a escuchar?
 app.listen(3001);
 //Mensaje de verificación
